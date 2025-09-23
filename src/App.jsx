e6922ee63588a5dc24404e5291cb624929a829e8
Rohan K Joseph <rohankj77@gmail.com>
2025-09-23 19:11:35 +0530

    loading page almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,20 @@
+import { useState } from 'react'
+import LoadingScreen from './components/LoadingPage'
  
+
+function App() {
+  const [loading, setLoading] = useState(true);
+
+  return (
+    <>
+      {loading && <LoadingScreen onFinish={() => setLoading(false)} />}
+      {!loading && (
+        <div className="App">
+          <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        </div>
+      )}
+    </>
+  );
+}
+
+export default App
